b53fa7ade3272f73d4b80d68b1739d5c87d0f2a1
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-26 18:28:47 -0500

    create vote route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const sequelize = require("../../config/connection");
+const { Post, User, Vote } = require("../../models");
 
 //get all posts
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -26,7 +38,18 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -59,6 +82,37 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.put("/upvote", (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  }).then(() => {
+    return Post.findOne({
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        //use raw SQL query
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+      where: {
+        id: req.body.post_id,
+      },
+    })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 });
 
 //update a post
@@ -106,4 +160,5 @@ router.delete("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
 module.exports = router;
