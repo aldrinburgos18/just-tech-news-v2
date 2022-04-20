99d6bf3be70bb8e058de46d6b606e0a4272b82f5
aldrinburgos <burgos.aldrin@gmail.com>
2022-04-19 20:02:59 -0500

    add front end login logic

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-function signupFormSubmitHandler(event) {
+async function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector("#username-signup").value.trim();
@@ -6,7 +6,7 @@ function signupFormSubmitHandler(event) {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && email && password) {
-    fetch("/api/users", {
+    const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
         username,
@@ -14,10 +14,43 @@ function signupFormSubmitHandler(event) {
         password,
       }),
       headers: { "Content-Type": "application/json" },
-    }).then((response) => console.log(response));
+    });
+    //check the response status
+    if (response.ok) {
+      console.log("User successfully created.");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
   }
 }
 
 document
   .querySelector(".signup-form")
-  .addEventListener("submit", signupFormSubmitHandler);
+  .addEventListener("submit", signupFormHandler);
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
