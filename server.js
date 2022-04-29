7a46666ab18c7acda6430cd398c421b554bddc0f
aldrinburgos <burgos.aldrin@gmail.com>
2022-04-29 18:30:09 -0500

    implement helpers

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const path = require("path");
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
