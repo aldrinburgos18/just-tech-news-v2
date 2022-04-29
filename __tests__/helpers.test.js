6bde081be2b201558a7d403fc665d673218ed636
aldrinburgos <burgos.aldrin@gmail.com>
2022-04-29 17:18:47 -0500

    add test and helper function for simple pluralization

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,14 @@
-const { format_date } = require("../utils/helpers");
+const { format_date, format_plural } = require("../utils/helpers");
 
-test("format date() returns a date string", () => {
+test("format_date() returns a date string", () => {
   const date = new Date("2022-04-23 20:46:00");
 
   expect(format_date(date)).toBe("4/23/2022");
+});
+
+test("format_plural() returns pluralized words based on amount", () => {
+  const word = "tiger";
+  const amount = 2;
+
+  expect(format_plural(word, amount)).toBe("tigers");
 });
