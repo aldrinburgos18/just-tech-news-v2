f7c13cd939c59940d0088579fa2046dca27ca049
aldrinburgos <burgos.aldrin@gmail.com>
2022-05-02 19:56:27 -0500

    display user posts on dashboard

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
+const dashboarRoutes = require("./dashboard-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
+router.use("/dashboard", dashboarRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
