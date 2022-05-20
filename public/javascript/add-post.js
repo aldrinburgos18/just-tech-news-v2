8684c22516de432bf8d04aa6cc377938af642873
aldrinburgos <burgos.aldrin@gmail.com>
2022-05-19 19:48:26 -0500

    add front end edit and delete functions

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,11 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
+  let post_url = document.querySelector('input[name="post-url"]').value;
+
+  if (post_url.split("/")[0] != "http:") {
+    post_url = `http://${post_url}`;
+  }
 
   const response = await fetch("/api/posts", {
     method: "POST",
